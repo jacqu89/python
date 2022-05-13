6301761b5315474314406ada77cf4808171103e7
jacqu89 <jacqu.play@gmail.com>
2022-05-13 14:33:51 +0200

    lv3

--- ex/interpolacja.py
+++ ex/interpolacja.py
@@ -1,3 +1,4 @@
+from operator import le
 import re
 from funkcje import readData,dataList
 
@@ -5,14 +6,30 @@ s = 5
 x = 2
 
 data = readData()
-print("wprowadzone dane:")
+lista = dataList(s)
+
+print("-"*50)
+print("Wprowadzone dane:")
 for i in data:
     print(i)
+print("-"*50)
 
 
-lista = dataList(s)
-print("\nlista x0, x1, .. xn:")
-for l in lista:
-    print(l)
 
+def wynik(x):
+    b=[]
+    for i in range(0,s-1):
+        a=1
+        k=1
+        for j in range(0,s-1):
+            a = a*(x-lista[0][j])
+            k = k*(data[s-1][1]-lista[0][j])
+        b.append(data[s-1-i][1]*(a/k))
+    #print(b)                       #wynik dla każdego nawiasu
+    w=0
+    for u in range(0,len(b)):
+        w = w+b[u]
+    return w
 
+g = wynik(x)
+print(g)
